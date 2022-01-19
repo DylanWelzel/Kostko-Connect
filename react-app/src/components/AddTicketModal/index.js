c440d349c9392348379ed43605fa749e5e0e4912
Dylan Welzel <dylanwelzel@gmail.com>
2022-01-19 14:50:41 -0800

    create a ticket functional and styled

---
+++ react-app/src/components/AddTicketModal/index.js
@@ -0,0 +1,89 @@
+import React from "react";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { postTicketThunk } from "../../store/tickets";
+import "./addticketmodal.css";
+
+const AddTicketModal = ({ setIsOpen }) => {
+    const [itemName, setItemName] = useState('')
+    const [location, setLocation] = useState('')
+    const [description, setDescription] = useState('')
+    const [errors, setErrors] = useState([]);
+    const { departmentId } = useParams()
+    const userId = useSelector((state) => state.session.user.id);
+    const dispatch = useDispatch()
+
+
+    const createTicket = async (e) => {
+        e.preventDefault()
+        const data = await dispatch(postTicketThunk(userId, departmentId, itemName, location, description))
+        if (data.errors) {
+            return setErrors(data.errors)
+        }
+        return setIsOpen(false)
+    }
+    return (
+        <>
+            <div className='darkBG' onClick={() => setIsOpen(false)} />
+            <div className='centered'>
+                <form onSubmit={createTicket} className='ticketModal'>
+                    <div className='modalHeader'>
+                        <h5 className='heading'>Add a Ticket</h5>
+                    </div>
+                    <div className='modalContent'>
+                        Item Name
+                    </div>
+                    <input
+                        required
+                        className='modalInput'
+                        type="text"
+                        value={itemName}
+                        onChange={(e) => setItemName(e.target.value)}
+                    />
+                    <div className='modalContent'>
+                        Location
+                    </div>
+                    <input
+                        required
+                        className='modalInput'
+                        type="text"
+                        value={location}
+                        onChange={(e) => setLocation(e.target.value)}
+                    />
+                    <div className='modalContent'>
+                        Description
+                    </div>
+                    <input
+                        required
+                        className='modalInput'
+                        type="text"
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                    />
+                    {errors && errors.map((error, ind) => (
+                        <div className='modalErrors' key={ind}>{error}</div>
+                    ))}
+                    <div className='modalActions'>
+                        <div className='actionsContainer'>
+                            <button
+                                type="submit"
+                                className='deleteBtn'
+                            >
+                                Add
+                            </button>
+                            <button
+                                className='cancelBtn'
+                                onClick={() => setIsOpen(false)}
+                            >
+                                Cancel
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div >
+        </>
+    );
+};
+
+export default AddTicketModal;
--- react-app/src/components/Tickets/index.js
+++ react-app/src/components/Tickets/index.js
@@ -1,39 +1,50 @@
 import "./tickets.css";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { deleteTicketThunk, editTicketThunk, postTicketThunk, readTicketsThunk } from "../../store/tickets";
 import SingleTicket from "../SingleTicket";
+import AddTicketModal from "../AddTicketModal";
 
 function Tickets() {
     const dispatch = useDispatch();
     const { departmentId } = useParams()
     const tickets = useSelector((state) => state.tickets);
+    const [isOpen, setIsOpen] = useState(false);
+    const [isEditOpen, setIsEditOpen] = useState(false);
+    const [editId, setEditId] = useState(null);
 
     useEffect(() => {
         dispatch(readTicketsThunk(departmentId))
     }, [dispatch])
 
     return (
-        <div className="ticketList">
-            {tickets && tickets.map(ticket => {
-                return (
-                    <div className="singleTicketContainer" key={ticket.id} >
-                        <SingleTicket
-                            departmentId={departmentId}
-                            ticketId={ticket.id}
-                            itemName={ticket.item_name}
-                            location={ticket.location}
-                            description={ticket.description}
-                            departmentId={ticket.department_id}
-                        />
-                    </div>
-                )
-            })}
-
-        </div>
+        <>
+            <div className="ticketList">
+                {tickets && tickets.map(ticket => {
+                    return (
+                        <div className="singleTicketContainer" key={ticket.id} >
+                            <SingleTicket
+                                departmentId={departmentId}
+                                ticketId={ticket.id}
+                                itemName={ticket.item_name}
+                                location={ticket.location}
+                                description={ticket.description}
+                                departmentId={ticket.department_id}
+                            />
+                        </div>
+                    )
+                })}
+            </div>
+            <div className="addItHereContainer">
+                <button className='primaryBtn' onClick={() => setIsOpen(true)}>
+                    Add a Ticket
+                </button>
+            </div>
+            {isOpen && <AddTicketModal setIsOpen={setIsOpen} />}
+            {/* {isEditOpen && <EditDepartModal editId={editId} setIsOpen={setIsEditOpen} />} */}
+        </>
     );
-
 }
 
 export default Tickets;
