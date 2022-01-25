bccf784e3b07fe9b56590f61889a9c6fee94d56f
Dylan Welzel <dylanwelzel@gmail.com>
2022-01-24 17:17:04 -0800

    departments polish

--- app/seeds/departments.py
+++ app/seeds/departments.py
@@ -3,12 +3,28 @@ from app.models import db, Department
 
 def seed_departments():
     department_1 = Department(name='Bakery')
-    department_2 = Department(name='Front Foods')
+    department_2 = Department(name='Seasonal')
     department_3 = Department(name='Deli')
+    department_4 = Department(name='Meat')
+    department_5 = Department(name='Liquor')
+    department_6 = Department(name='Frozen Food')
+    department_7 = Department(name='Office Supplies')
+    department_8 = Department(name='Tires')
+    department_9 = Department(name='Hardware')
+    department_10 = Department(name='Automotive')
+    department_11 = Department(name='Candy')
 
     db.session.add(department_1)
     db.session.add(department_2)
     db.session.add(department_3)
+    db.session.add(department_4)
+    db.session.add(department_5)
+    db.session.add(department_6)
+    db.session.add(department_7)
+    db.session.add(department_8)
+    db.session.add(department_9)
+    db.session.add(department_10)
+    db.session.add(department_11)
 
     db.session.commit()
 
--- app/seeds/tickets.py
+++ app/seeds/tickets.py
@@ -5,13 +5,28 @@ def seed_tickets():
     ticket_1 = Ticket(
         item_name='Flour', location='124 A4', description='6 bags, fronted', department_id=1, owner_id=1)
     ticket_2 = Ticket(
-        item_name='Lamps', location='125 B5', description='4 layers', department_id=2, owner_id=2)
+        item_name='Christmas Lights', location='125 B5', description='4 layers', department_id=2, owner_id=2)
     ticket_3 = Ticket(
-        item_name='Coffee', location='122 B1', description='8 layers', department_id=3, owner_id=3)
+        item_name='Pesto', location='122 B1', description='8 layers', department_id=3, owner_id=3)
+    ticket_4 = Ticket(
+        item_name='Chicken', location='120 B4', description='2 layers', department_id=4, owner_id=1)
+    ticket_5 = Ticket(
+        item_name='Jameson', location='149 A1', description='100 Bottles', department_id=5, owner_id=3)
+    ticket_6 = Ticket(
+        item_name='Pizza', location='242 C1', description='2 layers', department_id=6, owner_id=3)
+    ticket_7 = Ticket(
+        item_name='Dunder Mifflin Paper', location='219 I2', description='23 Reams', department_id=7, owner_id=3)
+    ticket_8 = Ticket(
+        item_name='Michelin', location='122 H4', description='8 layers', department_id=8, owner_id=3)
 
     db.session.add(ticket_1)
     db.session.add(ticket_2)
     db.session.add(ticket_3)
+    db.session.add(ticket_4)
+    db.session.add(ticket_5)
+    db.session.add(ticket_6)
+    db.session.add(ticket_7)
+    db.session.add(ticket_8)
 
     db.session.commit()
 
