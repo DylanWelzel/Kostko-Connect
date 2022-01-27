e6356f98bc261db0521c8cfc272c1126b25a02f1
Dylan Welzel <dylanwelzel@gmail.com>
2022-01-26 18:23:49 -0800

    pregrading localhost 5000

--- react-app/src/store/socket.js
+++ react-app/src/store/socket.js
@@ -8,12 +8,14 @@ export const getSocket = () => {
 }
 //"https://kostcoconnect.herokuapp.com/"
 //"http://localhost:5000/"
-const endPoint = "https://kostcoconnect.herokuapp.com/";
+const endPoint = "http://localhost:5000/";
 
 const socket = (state = null, action) => {
     switch (action.type) {
         case GET_SOCKET:
-            return io(`${endPoint}`);
+            return io(`${endPoint}`, {
+                reconnectionDelayMax: 10000
+            });
         default:
             return state
     }
