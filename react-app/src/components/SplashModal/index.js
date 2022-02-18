8fee48c049bace509fde9d9690874172620d2e85
Dylan Welzel <dylanwelzel@gmail.com>
2022-02-18 12:24:44 -0800

    content addition

--- react-app/src/components/SplashModal/index.js
+++ react-app/src/components/SplashModal/index.js
@@ -20,7 +20,7 @@ const SplashModal = ({ setIsOpen }) => {
                         <h5 className='heading'>What is Kostco Connect?</h5>
                     </div>
                     <div className='modalContent'>
-                        Kostco Connect is a web application inspired by my time working at a warehouse and seeing the need for an increase in productivity and communication. On signup there are 3 roles you can select. Admins (Demo User) can create Departments, Stockers can add Tickets and Drivers can mark Tickets as complete. Nested in each ticket is a live chat feature where a Driver and Stocker can communicate with one another about a certain ticket. Enjoy the application!
+                        Kostco Connect is a web application inspired by my time working at a warehouse and seeing the need for an increase in productivity and communication. On signup there are 3 roles you can select. Admins (Demo User) can create Departments, Stockers can add Tickets and Drivers can mark Tickets as complete. Nested in each ticket is a live chat feature where a Driver and Stocker can communicate with one another about a certain ticket. Play around and create your own Departments, add Tickets and write Messages! Enjoy the application!
                     </div>
                     <button
                         className='continueBtn'
